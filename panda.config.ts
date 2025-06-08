065182d36ed182f595c596b44d165a3abd152d2f
bh2980 <bh2980@naver.com>
2025-06-09 04:08:23 +0900

    feat: move prettey-code related css to panda.coonfig.ts

--- panda.config.ts
+++ panda.config.ts
@@ -86,6 +86,56 @@ export default defineConfig({
       },
       "& figure[data-rehype-pretty-code-figure]": {
         marginBottom: "1.5rem",
+
+        "&:has(figcaption[data-rehype-pretty-code-title])": {
+          "& figcaption[data-rehype-pretty-code-title]": {
+            backgroundColor: "rgb(36, 36, 36)",
+            padding: "0.7rem 1rem",
+            color: "rgb(207, 207, 207)",
+            borderRadius: "16px 16px 0 0",
+            borderBottom: "1px solid rgb(51 65 85)",
+          },
+          "& pre": {
+            borderRadius: "0 0 16px 16px",
+          },
+        },
+        "& pre": {
+          paddingTop: "1rem",
+          paddingBottom: "1rem",
+          borderRadius: "16px",
+          overflow: "auto",
+          "& code[data-line-numbers]": {
+            counterReset: "line",
+          },
+          "& code[data-line-numbers] > [data-line]::before": {
+            counterIncrement: "line",
+            content: "counter(line)",
+            display: "inline-block",
+            width: "0.75rem",
+            marginRight: "2rem",
+            textAlign: "right",
+            color: "gray",
+          },
+          "& code[data-line-numbers-max-digits='2'] > [data-line]::before": {
+            width: "1.25rem",
+          },
+          "& code[data-line-numbers-max-digits='3'] > [data-line]::before": {
+            width: "1.75rem",
+          },
+          "& span[data-line]": {
+            paddingLeft: "1rem",
+          },
+          "& span[data-highlighted-line]": {
+            backgroundColor: "rgb(51 65 85)",
+            borderLeft: "3px solid rgb(59 130 246)",
+            paddingLeft: "calc(1rem - 3px)",
+          },
+        },
+      },
+      "& :not(pre) > code": {
+        padding: "0.2rem 0.5rem",
+        borderRadius: "0.5rem",
+        marginRight: "0.25rem",
       },
     },
   },
