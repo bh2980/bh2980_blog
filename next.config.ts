53604f489eca087ecbd72efc41a3adc4bb8ad312
bh2980 <74360958+bh2980@users.noreply.github.com>
2025-07-18 11:43:40 +0900

    chore: next/mdx 관련 패키지 제거하고 next-mdx-remote 추가

--- next.config.ts
+++ next.config.ts
@@ -1,18 +1,7 @@
 import type { NextConfig } from "next";
 
-import nextMDX from "@next/mdx";
-
-const withMDX = nextMDX({
-  extension: /\.mdx?$/,
-  options: {
-    remarkPlugins: [],
-    rehypePlugins: [],
-  },
-});
-
 const nextConfig: NextConfig = {
-  pageExtensions: ["js", "jsx", "md", "mdx", "ts", "tsx"],
   reactStrictMode: true,
 };
 
-export default withMDX(nextConfig);
+export default nextConfig;
