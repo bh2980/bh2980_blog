9ff1b8a83f3c0e3e65cb139d2c8ed148e48ae30a
bh2980 <74360958+bh2980@users.noreply.github.com>
2025-08-15 21:05:02 +0900

    feat: add memo Snippets 카테고리 추가

--- content/categories.ts
+++ content/categories.ts
@@ -1,6 +1,2 @@
 export const POST_CATEGORIES = ["Frontend"] as const;
-export const MEMO_CATEGORIES = [
-  "알고리즘",
-  "Type Challenges",
-  "CSS Battle",
-] as const;
+export const MEMO_CATEGORIES = ["알고리즘", "Type Challenges", "CSS Battle", "Snippets"] as const;
