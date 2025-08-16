40728e2f3c0f4ea61b392b912a0e606376c62723
bh2980 <74360958+bh2980@users.noreply.github.com>
2025-08-16 12:06:11 +0900

    feat: memo category에 Crypto Zombies 추가

--- content/categories.ts
+++ content/categories.ts
@@ -1,2 +1,2 @@
 export const POST_CATEGORIES = ["Frontend", "Web3"] as const;
-export const MEMO_CATEGORIES = ["알고리즘", "Type Challenges", "CSS Battle", "Snippets"] as const;
+export const MEMO_CATEGORIES = ["Snippets", "알고리즘", "Type Challenges", "CSS Battle", "Crypto Zombies"] as const;
