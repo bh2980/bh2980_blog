02d59e6194529a191c0c16ef8159fa5d07a122a8
bh2980 <bh2980@naver.com>
2025-08-16 10:53:03 +0900

    refactor: 중복된 값들을 contentSchema으로 추출 및 각 schema가 contentSchema를 상속받도록 수정

--- velite/schemas.ts
+++ velite/schemas.ts
@@ -1,39 +1,27 @@
 import { s } from "velite";
-import { generateSlugFromFilename, validatePostSlugs } from "./utils";
+import { generateSlugFromFilename, getContentFilePathArray, getKoreaISOTimeText, validatePostSlugs } from "./utils";
 import { POST_CATEGORIES, MEMO_CATEGORIES } from "@/content/categories";
 
-export const postSchema = s.object({
+const contentSchema = s.object({
   title: s.string(),
   slug: s.string().default("").transform(generateSlugFromFilename),
-  createdAt: s.isodate(),
+  createdAt: s.isodate().default(getKoreaISOTimeText()),
+  path: s.array(s.string()).default([]).transform(getContentFilePathArray),
+});
+
+export const postSchema = contentSchema.extend({
   category: s.enum(POST_CATEGORIES),
   tags: s.array(s.string()),
   excerpt: s.excerpt({ length: 80 }),
 });
 
-export const seriesSchema = s.object({
-  title: s.string(),
-  slug: s.string().default("").transform(generateSlugFromFilename),
+export const seriesSchema = contentSchema.extend({
   description: s.string(),
   postSlugs: s.array(s.string()).refine(validatePostSlugs),
-  createdAt: s.isodate(),
 });
 
-export const memoSchema = s.object({
-  title: s.string(),
-  slug: s.string().default("").transform(generateSlugFromFilename),
-  createdAt: s.isodate(),
+export const memoSchema = contentSchema.extend({
   category: s.enum(MEMO_CATEGORIES),
   tags: s.array(s.string()),
   excerpt: s.excerpt({ length: 80 }),
-  path: s
-    .array(s.string())
-    .optional()
-    .transform((_, context) => {
-      if (Array.isArray(context.meta.history) && context.meta.history.length > 0) {
-        return (context.meta?.history[0] as string).replace(/^.*?\/content\//, "content/").split("/");
-      }
-
-      return [];
-    }),
 });
