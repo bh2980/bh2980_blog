20ec647ed593ad0fd28b8a541d117f85d50de0e3
bh2980 <bh2980@naver.com>
2025-08-16 10:56:12 +0900

    feat: post 카테고리에 Web3 추가

--- content/categories.ts
+++ content/categories.ts
@@ -1,2 +1,2 @@
-export const POST_CATEGORIES = ["Frontend"] as const;
+export const POST_CATEGORIES = ["Frontend", "Web3"] as const;
 export const MEMO_CATEGORIES = ["알고리즘", "Type Challenges", "CSS Battle", "Snippets"] as const;
