c20dc0e68c50e2f5cef550c132a20ecedd39a46d
bh2980 <bh2980@naver.com>
2025-12-24 00:19:46 +0900

    feat: keystatic에서 blog layout 제거

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -25,9 +25,7 @@ export default function RootLayout({
       <body
         className={`${pretendardVariable.variable} antialiased min-h-screen flex flex-col bg-white dark:bg-gray-950 text-gray-900 dark:text-gray-100`}
       >
-        <Navigation />
         <main className="flex-1">{children}</main>
-        <Footer />
       </body>
     </html>
   );
