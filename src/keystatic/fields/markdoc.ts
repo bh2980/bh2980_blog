51f5cb87208be551afb5a2ecd039a62f7864fbeb
bh2980 <74360958+bh2980@users.noreply.github.com>
2025-12-25 11:44:43 +0900

    feat: slug와 markdoc를 fields 파라미터와 통일

---
+++ src/keystatic/fields/markdoc.ts
@@ -0,0 +1,12 @@
+import { fields } from "@keystatic/core";
+
+import { mermaidBlock } from "../blocks/mermaid-block";
+
+export const markdoc = (_args: Parameters<typeof fields.markdoc>[0]) => {
+  return fields.markdoc({
+    ..._args,
+    components: _args.components ?? {
+      Mermaid: mermaidBlock,
+    },
+  });
+};
