b9248445e57a20fee9cd886da41d50e69c379d58
bh2980 <bh2980@naver.com>
2025-12-25 11:44:43 +0900

    feat: slug와 markdoc를 fields 파라미터와 통일

--- src/keystatic/fields/slug.ts
+++ src/keystatic/fields/slug.ts
@@ -1,21 +1,18 @@
 import { fields } from "@keystatic/core";
 
-export const slug = (nameLabel: string = '제목', slugLabel: string = '슬러그') =>
-  fields.slug({
-    name: { label: nameLabel },
+const koreanSlugGenerator = (name: string) =>
+  name
+    .trim()
+    .toLowerCase()
+    .replace(/\s+/g, "-") // 공백을 하이픈으로
+    .replace(/[^\w\uAC00-\uD7A3-]+/g, "");
+
+export const slug = (_args: Parameters<typeof fields.slug>[0]) => {
+  return fields.slug({
+    name: _args?.name,
     slug: {
-      label: slugLabel,
-      generate: (name) =>
-        name
-          .trim()
-          .toLowerCase()
-          .replace(/\s+/g, "-") // 공백을 하이픈으로
-          .replace(/[^\w\uAC00-\uD7A3-]+/g, ""), // 영문, 숫자, 한글(\uAC00-\uD7A3), 하이픈만 남기기
-      validation: {
-        pattern: {
-          regex: /^[a-z0-9\uAC00-\uD7A3-]+$/,
-          message: `${slugLabel}는 한글, 영문 소문자, 숫자, 하이픈만 사용할 수 있습니다.`,
-        },
-      },
+      ..._args?.slug,
+      generate: _args.slug?.generate ?? koreanSlugGenerator,
     },
   });
+};
