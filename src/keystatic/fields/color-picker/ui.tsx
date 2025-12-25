48ae0c7ba54c1b5e30f032d115de30485f175bc3
bh2980 <bh2980@naver.com>
2025-12-26 03:21:15 +0900

    feat: @keystar/ui 사용으로 발생한 production 런타임 오류 수정

--- src/keystatic/fields/color-picker/ui.tsx
+++ src/keystatic/fields/color-picker/ui.tsx
@@ -1,4 +1,3 @@
-import { FieldPrimitive } from "@keystar/ui/field";
 import type { FormFieldInputProps } from "@keystatic/core";
 
 type ColorPickerFieldInputProps = FormFieldInputProps<string> & {
@@ -8,8 +7,20 @@ type ColorPickerFieldInputProps = FormFieldInputProps<string> & {
 
 export const ColorPickerFieldInput = (props: ColorPickerFieldInputProps) => {
 	return (
-		<FieldPrimitive label={props.label} description={props.description} isRequired>
-			<input type="color" value={props.value} onChange={(e) => props.onChange(e.target.value)} />
-		</FieldPrimitive>
+		<label>
+			<div className="flex flex-col gap-1">
+				<span>
+					{props.label}
+					<div className="text-[var(--kui-color-foreground-critical)] text-2xl inline">*</div>
+				</span>
+				{!props.description && <span className="text-sm text-gray-500">{props.description}</span>}
+				<input
+					className="w-12 h-12"
+					type="color"
+					value={props.value}
+					onChange={(e) => props.onChange(e.target.value)}
+				/>
+			</div>
+		</label>
 	);
 };
