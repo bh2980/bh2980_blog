1ffdede748094924cf9984d5d9eaf8f5a95caffb
bh2980 <bh2980@naver.com>
2025-12-25 10:53:21 +0900

    feat: reader api 객체 추가

---
+++ src/libs/reader.ts
@@ -0,0 +1,15 @@
+import keystaticConfig from "@/root/keystatic.config";
+import { createReader } from "@keystatic/core/reader";
+import { createGitHubReader } from "@keystatic/core/reader/github";
+
+export const reader = (() => {
+  if (process.env.NODE_ENV === "development") {
+    return createReader(process.cwd(), keystaticConfig);
+  }
+
+  if (!process.env.GITHUB_PAT) {
+    throw new Error("GITHUB_PAT is not defined");
+  }
+
+  return createGitHubReader(keystaticConfig, { repo: "bh2980/bh2980_blog", token: process.env.GITHUB_PAT });
+})();
