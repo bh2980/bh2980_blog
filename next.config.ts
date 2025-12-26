0515a2d0355971d76a1d5e9557ba7f8020ca09be
bh2980 <bh2980@naver.com>
2025-12-27 02:50:20 +0900

    feat: typedRoutes 활성화

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
 	reactStrictMode: true,
 	transpilePackages: ["next-mdx-remote"],
+	typedRoutes: true,
 };
 
 export default nextConfig;
