f7fda2719a67874ccba63125308cb204bf000efa
bh2980 <bh2980@naver.com>
2025-12-27 15:33:18 +0900

    feat: blog layout에 Footer 추가

--- src/app/(blog)/(content)/layout.tsx
+++ src/app/(blog)/(content)/layout.tsx
@@ -1,4 +1,5 @@
 import { Fragment } from "react";
+import Footer from "@/components/footer";
 import Navigation from "@/components/navigation";
 
 export default function RootLayout({
@@ -10,6 +11,7 @@ export default function RootLayout({
 		<Fragment>
 			<Navigation className="sticky top-0 z-10 border-gray-200 border-b bg-white dark:border-gray-800 dark:bg-gray-950" />
 			<main className="flex-1">{children}</main>
+			<Footer />
 		</Fragment>
 	);
 }
