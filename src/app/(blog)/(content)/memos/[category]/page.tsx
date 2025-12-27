2b81f588608daada9ebea67ea7c4f44093fb1bc6
bh2980 <bh2980@naver.com>
2025-12-27 20:38:04 +0900

    feat: category route 구현

--- src/app/(blog)/(content)/memos/[category]/page.tsx
+++ src/app/(blog)/(content)/memos/[category]/page.tsx
@@ -1,13 +1,13 @@
 import { getMemoCategoryList } from "@/libs/contents/category";
 import { getMemoList } from "@/libs/contents/memo";
-import { MemoList } from "./memo-list";
+import { MemoList } from "../memo-list";
 
-export default async function MemoPage({ params }: { params: Promise<{ category: string }> }) {
+export default async function MemoCategoryPage({ params }: { params: Promise<{ category: string }> }) {
 	const category = (await params).category;
 
 	const categoryList = await getMemoCategoryList();
 
-	const memoList = await getMemoList({ category });
+	const memoList = await getMemoList();
 
 	return <MemoList currentCategory={category} categoryList={categoryList} memoList={memoList} />;
 }
