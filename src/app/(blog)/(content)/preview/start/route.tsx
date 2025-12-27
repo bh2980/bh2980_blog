d110bade9b6502c8fa05a5b75b18a934261cfc86
bh2980 <74360958+bh2980@users.noreply.github.com>
2025-12-28 02:53:27 +0900

    로컬에서 프리뷰 버튼 클릭 시 저장된 페이지를 띄워주도록 수정 (#16)

--- src/app/(blog)/(content)/preview/start/route.tsx
+++ src/app/(blog)/(content)/preview/start/route.tsx
@@ -1,21 +1,29 @@
 import { draftMode } from "next/headers";
 import { NextResponse } from "next/server";
+import keystaticConfig from "@/root/keystatic.config";
 
 export async function GET(req: Request) {
 	const url = new URL(req.url);
-	const branch = url.searchParams.get("branch");
-	const to = url.searchParams.get("to");
+	const origin = url.origin;
 
-	if (!branch || !to) {
-		return new Response("Missing branch or to params", { status: 400 });
+	const to = url.searchParams.get("to");
+	if (!to) return new Response("Missing branch or to params", { status: 400 });
+
+	const toUrl = new URL(to, origin);
+	if (toUrl.origin !== origin) {
+		return new Response("Invalid redirect URL", { status: 400 });
 	}
 
-	const dm = await draftMode();
-	dm.enable();
+	if (keystaticConfig.storage.kind === "local") {
+		return NextResponse.redirect(toUrl.toString());
+	}
 
-	const toUrl = new URL(to, url.origin);
+	const branch = url.searchParams.get("branch");
+	if (!branch) return new Response("Missing branch or to params", { status: 400 });
+
+	(await draftMode()).enable();
+
 	const res = NextResponse.redirect(toUrl.toString());
 	res.cookies.set("ks-branch", branch, { path: "/" });
-
 	return res;
 }
