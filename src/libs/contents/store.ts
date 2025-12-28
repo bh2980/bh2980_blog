550d4c11d74374e67f68f444d96b78d07b90cecd
bh2980 <bh2980@naver.com>
2025-12-29 02:32:36 +0900

    feat: 메모 및 포스트 카테고리 파일 삭제 및 콘텐츠 맵 구성 개선

--- src/libs/contents/store.ts
+++ src/libs/contents/store.ts
@@ -2,29 +2,33 @@ import "server-only";
 
 import { cache } from "react";
 import { reader } from "@/keystatic/libs/reader";
+import type { CollectionEntry, MemoEntry, PostEntry, TagEntry } from "@/keystatic/types";
 
 const buildSlugMap = <T extends object>(items: Array<{ slug: string; entry: T }>) => {
 	return new Map<string, T & { slug: string }>(items.map((item) => [item.slug, { ...item.entry, slug: item.slug }]));
 };
 
-const buildContentMap = async () => {
+const buildContentMap = async (): Promise<{
+	postMap: Map<string, PostEntry>;
+	memoMap: Map<string, MemoEntry>;
+	tagMap: Map<string, TagEntry>;
+	collectionMap: Map<string, CollectionEntry>;
+}> => {
 	const r = await reader();
 
-	const [posts, postCategories, memos, memoCategories, tags] = await Promise.all([
+	const [posts, memos, tags, collection] = await Promise.all([
 		r.collections.post.all(),
-		r.collections.postCategory.all(),
 		r.collections.memo.all(),
-		r.collections.memoCategory.all(),
 		r.collections.tag.all(),
+		r.collections.collection.all(),
 	]);
 
 	const postMap = buildSlugMap(posts);
-	const postCategoryMap = buildSlugMap(postCategories);
 	const memoMap = buildSlugMap(memos);
-	const memoCategoryMap = buildSlugMap(memoCategories);
 	const tagMap = buildSlugMap(tags);
+	const collectionMap = buildSlugMap(collection);
 
-	return { postMap, postCategoryMap, memoMap, memoCategoryMap, tagMap };
+	return { postMap, memoMap, tagMap, collectionMap };
 };
 
 export const getContentMap = cache(() => buildContentMap());
