3424b1b0164678383375ee485fab9cc7568b7f25
bh2980 <bh2980@naver.com>
2025-12-29 02:50:41 +0900

    feat: keystaticCollection 타입에 ComponentSchema 추가 및 EntryWithSlug 타입 수정

--- src/keystatic/types.ts
+++ src/keystatic/types.ts
@@ -1,10 +1,12 @@
-import type { Collection as keystaticCollection } from "@keystatic/core";
+import type { ComponentSchema, Collection as keystaticCollection } from "@keystatic/core";
 import type { Entry } from "@keystatic/core/reader";
 import type keystaticConfig from "@/root/keystatic.config";
 import type { Expand } from "@/utils/types";
 import type { MemoCategory, PostCategory } from "./collections";
 
-type EntryWithSlug<T extends keystaticCollection<any, any>> = Entry<T> & { slug: string };
+type EntryWithSlug<T extends keystaticCollection<Record<string, ComponentSchema>, string>> = Entry<T> & {
+	slug: string;
+};
 
 export type TagEntry = Expand<EntryWithSlug<typeof keystaticConfig.collections.tag>>;
 export type MemoEntry = Expand<EntryWithSlug<typeof keystaticConfig.collections.memo>>;
