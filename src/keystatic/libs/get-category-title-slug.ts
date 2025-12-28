7fba9389bd8a6d9c38e9a599556b58b266bf8fde
bh2980 <bh2980@naver.com>
2025-12-28 21:40:49 +0900

    fix: 카테고리 입력값이 없을 경우 대체값을 설정하는 로직 추가

--- src/keystatic/libs/get-category-title-slug.ts
+++ src/keystatic/libs/get-category-title-slug.ts
@@ -7,7 +7,16 @@ export const getCategoryTitleSlug = (name: string) => {
 	const forId = (labelElement as HTMLLabelElement).htmlFor;
 	const categoryInput = document.getElementById(forId) as HTMLInputElement;
 
-	const category = categoryInput?.value;
+	let category: string | undefined = categoryInput?.value;
+
+	if (!forId || !categoryInput) {
+		const parentElement = labelElement.parentElement;
+		const categoryInput = parentElement?.querySelector("input") as HTMLInputElement | null;
+		const selectButton = parentElement?.querySelector('button[aria-haspopup="listbox"]') as HTMLButtonElement | null;
+
+		category = categoryInput?.value || selectButton?.textContent;
+	}
+
 	const title = getKoreanSlug(name);
 
 	if (!category || !title) return "";
