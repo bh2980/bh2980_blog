71ef13886b675a43ce6b3ab872c37be642bf406e
bh2980 <bh2980@naver.com>
2025-12-29 02:30:22 +0900

    feat: 슬러그를 포함한 새로운 타입 정의 및 카테고리 타입 추가

--- src/keystatic/types.ts
+++ src/keystatic/types.ts
@@ -1,11 +1,17 @@
+import type { Collection as keystaticCollection } from "@keystatic/core";
 import type { Entry } from "@keystatic/core/reader";
 import type keystaticConfig from "@/root/keystatic.config";
 import type { Expand } from "@/utils/types";
+import type { MemoCategory, PostCategory } from "./collections";
 
-type withSlug<T extends object> = T & { slug: string };
+type EntryWithSlug<T extends keystaticCollection<any, any>> = Entry<T> & { slug: string };
 
-export type MemoEntry = Expand<withSlug<Entry<typeof keystaticConfig.collections.memo>>>;
-export type PostEntry = Expand<withSlug<Entry<typeof keystaticConfig.collections.post>>>;
-export type MemoCategoryEntry = Expand<withSlug<Entry<typeof keystaticConfig.collections.memoCategory>>>;
-export type PostCategoryEntry = Expand<withSlug<Entry<typeof keystaticConfig.collections.postCategory>>>;
-export type TagEntry = Expand<withSlug<Entry<typeof keystaticConfig.collections.tag>>>;
+export type TagEntry = Expand<EntryWithSlug<typeof keystaticConfig.collections.tag>>;
+export type MemoEntry = Expand<EntryWithSlug<typeof keystaticConfig.collections.memo>>;
+export type PostEntry = Expand<EntryWithSlug<typeof keystaticConfig.collections.post>>;
+export type CollectionEntry = Expand<EntryWithSlug<typeof keystaticConfig.collections.collection>>;
+
+export type Tag = TagEntry;
+export type Memo = Expand<Omit<MemoEntry, "tags" | "category"> & { category: MemoCategory; tags: Tag[] }>;
+export type Post = Expand<Omit<PostEntry, "tags" | "category"> & { category: PostCategory; tags: Tag[] }>;
+export type Collection = Expand<Entry<typeof keystaticConfig.collections.collection>>;
