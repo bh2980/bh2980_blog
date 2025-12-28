137d7eef3cad09b373af7b8069ffb5ea5326626c
bh2980 <bh2980@naver.com>
2025-12-29 00:33:17 +0900

    feat: 메모 및 포스트 카테고리 컬렉션에 설명 필드 추가

--- src/keystatic/collections/memo-category.collection.ts
+++ src/keystatic/collections/memo-category.collection.ts
@@ -9,6 +9,7 @@ export const memoCategoryCollection = collection({
 		name: fields.slug({
 			name: { label: "카테고리 이름", validation: { isRequired: true } },
 		}),
+		description: fields.text({ label: "설명" }),
 		color: fields.colorPicker({ label: "색상", defaultValue: "#000000" }),
 	},
 });
