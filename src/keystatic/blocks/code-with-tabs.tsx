6058c267aecd84646f25f6f2d8fcf21c1410f9da
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2025-12-30 07:52:55 +0900

    PureMdx 블럭 추가 및 remarkCode* 에러 해결 (#21)

--- src/keystatic/blocks/code-with-tabs.tsx
+++
@@ -1,9 +0,0 @@
-import { fields } from "@keystatic/core";
-import { wrapper } from "@keystatic/core/content-components";
-import { CodeBlock } from "../fields/code-block/ui";
-
-export const codeWithTabsBlock = wrapper({
-	label: "Code with Tabs",
-	schema: { mdx: fields.text({ label: "mdx", multiline: true }) },
-	ContentView: (props) => <CodeBlock codeblock={{ value: props.value.mdx, lang: "md", meta: "" }} />,
-});
--- src/keystatic/fields/code-block/index.tsx
+++ src/keystatic/fields/code-block/index.tsx
@@ -1,7 +1,7 @@
 import type { BasicFormField } from "@keystatic/core";
 import type { RawCode } from "codehike/code";
 import { basicFormFieldWithSimpleReaderParse, FieldDataError } from "@/keystatic/libs/custom-field";
-import { CodeInput } from "./ui";
+import { CodeEditor, CodeEditorWithLangSelector } from "./code-editor";
 
 export const codeBlockField = ({
 	label,
@@ -13,13 +13,45 @@ export const codeBlockField = ({
 	return basicFormFieldWithSimpleReaderParse({
 		label,
 		Input(props) {
-			return <CodeInput {...props} />;
+			return <CodeEditor {...props} />;
 		},
 		defaultValue() {
-			return { value: "", lang: "ts", meta: "" };
+			return { value: "", lang: "typescript", meta: "" };
 		},
 		parse(value) {
-			if (value === undefined) return { lang: "ts", value: "", meta: "" };
+			if (value === undefined) return { lang: "typescript", value: "", meta: "" };
+			return value as RawCode;
+		},
+		validate(value) {
+			if (validation?.isRequired && !value.value) {
+				throw new FieldDataError("Must not be empty");
+			}
+
+			return value;
+		},
+		serialize(value) {
+			return { value };
+		},
+	});
+};
+
+export const multiLangCodeBlockField = ({
+	label,
+	validation,
+}: {
+	label: string;
+	validation?: { isRequired: boolean };
+}): BasicFormField<RawCode> => {
+	return basicFormFieldWithSimpleReaderParse({
+		label,
+		Input(props) {
+			return <CodeEditorWithLangSelector {...props} />;
+		},
+		defaultValue() {
+			return { value: "", lang: "typescript", meta: "" };
+		},
+		parse(value) {
+			if (value === undefined) return { lang: "typescript", value: "", meta: "" };
 			return value as RawCode;
 		},
 		validate(value) {
