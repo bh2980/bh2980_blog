78bfa8758764a87da0f23babdec851930e9127d5
bh2980 <74360958+bh2980@users.noreply.github.com>
2025-12-29 17:37:07 +0900

    codehike에서 추가못한 핸들러 추가 (#18)

---
+++ src/keystatic/fields/code-block/index.tsx
@@ -0,0 +1,36 @@
+import type { BasicFormField } from "@keystatic/core";
+import type { RawCode } from "codehike/code";
+import { basicFormFieldWithSimpleReaderParse, FieldDataError } from "@/keystatic/libs/custom-field";
+import { CodeInput } from "./ui";
+
+export const codeBlockField = ({
+	label,
+	validation,
+}: {
+	label: string;
+	validation?: { isRequired: boolean };
+}): BasicFormField<RawCode> => {
+	return basicFormFieldWithSimpleReaderParse({
+		label,
+		Input(props) {
+			return <CodeInput {...props} />;
+		},
+		defaultValue() {
+			return { value: "", lang: "ts", meta: "" };
+		},
+		parse(value) {
+			if (value === undefined) return { lang: "ts", value: "", meta: "" };
+			return value as RawCode;
+		},
+		validate(value) {
+			if (validation?.isRequired && !value.value) {
+				throw new FieldDataError("Must not be empty");
+			}
+
+			return value;
+		},
+		serialize(value) {
+			return { value };
+		},
+	});
+};
