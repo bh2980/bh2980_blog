32df120d2d8221fcda2a3642ae74091ee662f48f
bh2980 <bh2980@naver.com>
2025-12-31 18:19:09 +0900

    feat: 게시글 목록 컴포넌트에 Separator 추가 및 레이아웃 수정

--- src/app/(blog)/(content)/posts/post-list.tsx
+++ src/app/(blog)/(content)/posts/post-list.tsx
@@ -1,4 +1,6 @@
 import Link from "next/link";
+import { Fragment } from "react/jsx-runtime";
+import { Separator } from "@/components/ui/separator";
 import type { ListResult, Post, PostCategoryListMeta, PostCategoryWithCount } from "@/libs/contents/types";
 import { cn } from "@/utils/cn";
 
@@ -12,12 +14,12 @@ export const PostList = ({
 	postList: ListResult<Post>;
 }) => {
 	return (
-		<div className="mx-auto max-w-4xl px-4 py-12 sm:px-6 lg:px-8">
-			<div className="mb-12">
+		<div className="mx-auto max-w-3xl px-4 py-12 sm:px-6 lg:px-8">
+			<div className="mb-6">
 				<h1 className="mb-4 font-bold text-3xl text-gray-900 dark:text-gray-100">블로그</h1>
-				<p className="mb-8 text-gray-600 dark:text-gray-300">개발하면서 배운 것들과 경험을 기록합니다.</p>
+				<p className="mb-6 text-gray-600 dark:text-gray-300">개발하면서 배운 것들과 경험을 기록합니다.</p>
 
-				<div className="mb-8 flex flex-wrap gap-2">
+				<div className="mb-6 flex flex-wrap gap-2">
 					<Link
 						href={"/posts"}
 						className={cn(
@@ -56,14 +58,26 @@ export const PostList = ({
 					<p className="text-gray-500 text-lg dark:text-gray-400">아직 작성된 게시글이 없습니다.</p>
 				</div>
 			) : (
-				<div className="flex flex-col gap-2">
-					{postList.list.map((post) => (
-						<Link key={post.slug} href={`/posts/${post.slug}`} className="block">
-							<article className="flex h-full items-center gap-4 rounded-lg p-4">
-								<time className="w-16 text-end text-gray-500 text-xs dark:text-gray-400">{post.publishedDate}</time>
-								<h2 className="line-clamp-2 font-semibold dark:text-gray-300">{post.title}</h2>
-							</article>
-						</Link>
+				<div className="flex flex-col gap-1">
+					{postList.list.map((post, index) => (
+						<Fragment key={post.slug}>
+							{index !== 0 && <Separator />}
+							<Link
+								key={post.slug}
+								href={`/posts/${post.slug}`}
+								className="block rounded-md hover:bg-zinc-100 dark:hover:bg-zinc-800"
+							>
+								<article className="flex h-full flex-col gap-4 rounded-lg p-4">
+									<span className="flex gap-2 text-gray-500 text-xs dark:text-gray-400">
+										<span>{post.category.label}</span>
+										<span>·</span>
+										<time>{post.publishedDate}</time>
+									</span>
+									<h2 className="line-clamp-1 font-semibold text-xl dark:text-gray-300">{post.title}</h2>
+									<p className="line-clamp-2">{post.excerpt}</p>
+								</article>
+							</Link>
+						</Fragment>
 					))}
 				</div>
 			)}
