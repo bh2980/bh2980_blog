9a116ca96fe74a02f41dda9aa2142103a948ba5b
bh2980 <74360958+bh2980@users.noreply.github.com>
2026-01-02 00:13:01 +0900

    keystatic 브랜치 prefix 추가 및 블로그 환경변수 추가 (#26)

---
+++ env.d.ts
@@ -0,0 +1,7 @@
+declare namespace NodeJS {
+	interface ProcessEnv {
+		GITHUB_PAT: string;
+		NEXT_PUBLIC_KEYSTATIC_OWNER: string;
+		NEXT_PUBLIC_KEYSTATIC_REPO: string;
+	}
+}
--- src/keystatic/collections/memo.collection.ts
+++ src/keystatic/collections/memo.collection.ts
@@ -6,7 +6,7 @@ export const MEMO_CATEGORY_LIST = [
 	{ label: "학습 노트", value: "study-notes" },
 	{ label: "문제 풀이", value: "problem-solving" },
 	{ label: "스니펫", value: "snippets" },
-	{ label: "트러불슈팅", value: "troubleshooting" },
+	{ label: "트러블슈팅", value: "troubleshooting" },
 ] as const;
 
 export type MemoCategory = (typeof MEMO_CATEGORY_LIST)[number];
