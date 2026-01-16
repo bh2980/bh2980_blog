1878c5f4a79a93b7be448dd1f058a43e0b6e2f4b
bh2980 <bh2980@naver.com>
2026-01-16 20:26:09 +0900

    feat: 목차 컴포넌트 구현 및 활성화된 항목 감지 기능 추가

--- src/components/table-of-contents.client.tsx
+++ src/components/table-of-contents.client.tsx
@@ -1,5 +1,6 @@
 "use client";
 
+import { useEffect, useState } from "react";
 import type { ClassNameValue } from "tailwind-merge";
 import { cn } from "../utils";
 
@@ -10,6 +11,8 @@ export const TableOfContents = ({
 	contents: { id: string; level: number; content: string }[];
 	className?: ClassNameValue;
 }) => {
+	const [activeId, setActiveId] = useState<string>();
+
 	const handleTocItemSelect = (
 		e: React.MouseEvent<HTMLUListElement, MouseEvent> | React.KeyboardEvent<HTMLUListElement>,
 	) => {
@@ -22,10 +25,30 @@ export const TableOfContents = ({
 		headingElement?.scrollIntoView();
 		history.pushState(null, "", `#${headingId}`);
 	};
+
+	useEffect(() => {
+		const observer = new IntersectionObserver((entries) => {
+			entries.forEach((entry) => {
+				if (entry.isIntersecting) {
+					setActiveId(entry.target.id);
+				}
+			});
+		});
+		contents
+			.map((item) => document.getElementById(item.id))
+			.forEach((element) => {
+				if (!element) return;
+				observer.observe(element);
+			});
+
+		return () => observer.disconnect();
+	}, [contents]);
+
 	return (
 		<ul
 			className={cn(
-				"not-prose flex flex-col gap-2 rounded-md bg-muted p-5 text-muted-foreground text-sm [&_li]:cursor-pointer [&_li]:hover:text-accent-foreground",
+				"not-prose flex flex-col gap-2 rounded-md bg-slate-100 p-5 text-slate-500 text-sm",
+				"dark:bg-slate-800 dark:text-slate-400",
 				className,
 			)}
 			onClick={handleTocItemSelect}
@@ -34,6 +57,10 @@ export const TableOfContents = ({
 			{contents.map((item) => (
 				<li
 					key={item.id}
+					className={cn(
+						"cursor-pointer hover:text-accent-foreground",
+						activeId === item.id && "font-bold text-slate-600 dark:text-slate-300",
+					)}
 					style={{
 						marginLeft: `${item.level * 12}px`,
 					}}
