6f38bb7aba2e5ebae687b4a5ad49a5b76c914497
bh2980 <bh2980@naver.com>
2026-01-16 23:54:23 +0900

    fix: 링크 컴포넌트 'a'의 다크 모드 스타일 및 아이콘 색상 수정

--- src/components/mdx/a.tsx
+++ src/components/mdx/a.tsx
@@ -23,13 +23,14 @@ export const a = ({ children, href, ...props }: ComponentPropsWithRef<"a">) => {
 				"group inline-flex items-center",
 				"text-current decoration-slate-300/50 underline-offset-4",
 				"hover:text-slate-900 hover:decoration-slate-900",
+				"dark:decoration-slate-500/50 dark:hover:text-slate-300 dark:hover:decoration-slate-300",
 				props.className,
 			)}
 			target={props.target ?? "_blank"}
 			rel={props.rel ?? "noreferrer noopener"}
 		>
 			{children}
-			<ArrowUpRight className="mr-1 size-4 text-slate-400/60 group-hover:text-slate-900" />
+			<ArrowUpRight className="mr-1 size-4 text-slate-400/60 group-hover:text-slate-900 dark:group-hover:text-slate-300" />
 		</a>
 	);
 };
