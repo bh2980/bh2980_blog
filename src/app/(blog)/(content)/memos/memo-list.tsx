05cc92d3b28d8f6f7b773707c42ac10b8428fd61
bh2980 <bh2980@naver.com>
2026-01-21 00:33:10 +0900

    refactor: 메모 목록 컴포넌트에서 태그 필터의 기본값 및 쿼리 파라미터 추가

--- src/app/(blog)/(content)/memos/memo-list.tsx
+++ src/app/(blog)/(content)/memos/memo-list.tsx
@@ -1,14 +1,14 @@
 "use client";
 
 import Link from "next/link";
-import { parseAsArrayOf, parseAsString, useQueryState } from "nuqs";
+import { parseAsNativeArrayOf, parseAsString, useQueryState } from "nuqs";
 import { Badge } from "@/components/ui/badge";
 import { MultiSelect } from "@/components/ui/multi-select";
 import { Separator } from "@/components/ui/separator";
 import type { ListResult, Memo, Tag } from "@/libs/contents/types";
 
 export const MemoList = ({ memos, tags }: { tags: ListResult<Tag>; memos: ListResult<Omit<Memo, "content">> }) => {
-	const [tagFilter, setTagFilter] = useQueryState<string[]>("tags", parseAsArrayOf(parseAsString));
+	const [tagFilter, setTagFilter] = useQueryState<string[]>("tags", parseAsNativeArrayOf(parseAsString));
 
 	const memoList = memos.list.filter(
 		(memo) => tagFilter?.every((tag) => memo.tags.find((memoTag) => memoTag.slug === tag)) ?? true,
@@ -18,7 +18,12 @@ export const MemoList = ({ memos, tags }: { tags: ListResult<Tag>; memos: ListRe
 		<div className="mx-auto w-full max-w-3xl px-4 py-12 sm:px-6 lg:px-8">
 			<div className="mb-6">
 				<h1 className="mb-4 font-bold text-3xl text-slate-900 dark:text-slate-100">메모</h1>
-				<MultiSelect options={tags.list} onValueChange={setTagFilter} />
+				<MultiSelect
+					options={tags.list}
+					onValueChange={setTagFilter}
+					defaultValue={tagFilter}
+					placeholder="태그 선택"
+				/>
 			</div>
 
 			{memoList.length === 0 ? (
@@ -31,7 +36,7 @@ export const MemoList = ({ memos, tags }: { tags: ListResult<Tag>; memos: ListRe
 						<li key={memo.slug}>
 							{index !== 0 && <Separator className="my-1" />}
 							<Link
-								href={{ pathname: `/memos/${memo.slug}` }}
+								href={{ pathname: `/memos/${memo.slug}`, query: { tags: tagFilter } }}
 								className="block rounded-md hover:bg-slate-100 dark:hover:bg-slate-800"
 							>
 								<article className="flex h-full flex-col gap-3 rounded-lg p-4">
