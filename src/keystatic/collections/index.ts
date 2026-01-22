7e2acf644315725ebd9cf7a1f4678119f894d630
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2026-01-22 15:47:49 +0900

    태그 선택 추가 및 메모 카테고리 제거 (#61)

--- src/keystatic/collections/index.ts
+++ src/keystatic/collections/index.ts
@@ -1,6 +1,5 @@
 export * from "./collection.collection";
 export * from "./memo.collection";
-export * from "./memo-category.collection";
 export * from "./post.collection";
 export * from "./post-category.collection";
 export * from "./tag.collection";
