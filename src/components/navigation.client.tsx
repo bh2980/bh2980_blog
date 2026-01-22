bc9c09e7828fe15cabd7bb873098d8298547b9e1
bh2980 <bh2980@naver.com>
2026-01-22 19:56:02 +0900

    style: 내비게이션 컴포넌트의 패딩 수정

--- src/components/navigation.client.tsx
+++ src/components/navigation.client.tsx
@@ -64,7 +64,7 @@ export default function Navigation({ className }: NavigationProps) {
 
 	return (
 		<header className={cn("sticky top-0 z-50", className)}>
-			<div className="mx-auto flex h-14 max-w-2xl items-center justify-between px-4">
+			<div className="mx-auto flex h-14 max-w-2xl items-center justify-between px-4 md:px-0">
 				<Link
 					href="/"
 					className={cn(
