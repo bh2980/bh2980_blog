543c8edeea6207775f1314522c71ef61ed360eef
bh2980 <bh2980@naver.com>
2026-01-25 18:31:05 +0900

    refactor: Columns 컴포넌트의 레이아웃을 flex-col로 변경하여 반응형 디자인 개선

--- src/components/mdx/columns.tsx
+++ src/components/mdx/columns.tsx
@@ -1,7 +1,11 @@
 import type { PropsWithChildren } from "react";
 
 export const Columns = (props: PropsWithChildren) => {
-	return <div className="flex gap-1 [&>_*]:min-w-0 [&>_*]:flex-1 [&_img]:m-0! [&_p]:m-0!">{props.children}</div>;
+	return (
+		<div className="flex flex-col items-center gap-1 md:flex-row md:items-start [&>_*]:min-w-0 [&>_*]:flex-1 [&_img]:m-0! [&_p]:m-0!">
+			{props.children}
+		</div>
+	);
 };
 
 export const Column = (props: PropsWithChildren) => {
