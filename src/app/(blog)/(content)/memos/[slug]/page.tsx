07fa409cf82378f3a504d9fa645995877f7ce7b7
bh2980 <bh2980@naver.com>
2026-01-26 09:41:06 +0900

    fix: toc가 없을 때 0으로 나오는 문제 수정

--- src/app/(blog)/(content)/memos/[slug]/page.tsx
+++ src/app/(blog)/(content)/memos/[slug]/page.tsx
@@ -85,12 +85,12 @@ export default async function MemoPage({ params, searchParams }: MemoPageProps) 
 							))}
 						</ul>
 					</header>
-					{toc?.length && <TableOfContents toc={toc} className="mt-4 xl:hidden" />}
+					{toc?.length !== 0 ? <TableOfContents toc={toc} className="mt-4 xl:hidden" /> : null}
 					{content}
 				</article>
 			</div>
 			<aside className="hidden xl:block">
-				{toc?.length && <TableOfContents toc={toc} className="sticky top-22 max-w-68" />}
+				{toc?.length !== 0 ? <TableOfContents toc={toc} className="sticky top-22 max-w-68" /> : null}
 			</aside>
 		</div>
 	);
--- src/app/(blog)/(content)/posts/[slug]/page.tsx
+++ src/app/(blog)/(content)/posts/[slug]/page.tsx
@@ -119,7 +119,7 @@ export default async function BlogPost({ params, searchParams }: BlogPageProps) 
 								)}
 							</aside>
 						))}
-					{toc?.length && <TableOfContents toc={toc} className="mt-4 xl:hidden" />}
+					{toc?.length !== 0 ? <TableOfContents toc={toc} className="mt-4 xl:hidden" /> : null}
 					{content}
 				</article>
 
@@ -155,7 +155,7 @@ export default async function BlogPost({ params, searchParams }: BlogPageProps) 
 				<Comments slug={slug} />
 			</div>
 			<aside className="hidden xl:block">
-				{toc?.length && <TableOfContents toc={toc} className="sticky top-22 max-w-68" />}
+				{toc?.length !== 0 ? <TableOfContents toc={toc} className="sticky top-22 max-w-68" /> : null}
 			</aside>
 		</div>
 	);
