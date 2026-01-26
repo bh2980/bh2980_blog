c83dde96fcb3490db77759597f0c5421d0db6733
bh2980 <bh2980@naver.com>
2026-01-26 23:19:55 +0900

    feat: keystatic에 추가 플러그인 기능 구현

--- src/app/(admin)/keystatic/keystatic.ts
+++ src/app/(admin)/keystatic/keystatic.ts
@@ -1,6 +1,9 @@
 "use client";
 
 import { makePage } from "@keystatic/next/ui/app";
+import { makeExtraPlugins } from "@/keystatic/libs/plugins";
 import config from "../../../../keystatic.config";
+
+(globalThis as any).__KEYSTATIC_EXTRA_PM_PLUGINS__ = (schema: any) => makeExtraPlugins(schema);
 
 export default makePage(config);
