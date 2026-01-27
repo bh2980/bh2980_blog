bb2f694885b1baa54619f00954a02ebd1c8307d1
bh2980 <bh2980@naver.com>
2026-01-27 22:52:53 +0900

    feat: keystatic 플러그인 폴더를 keystatic root로 이동

--- global.d.ts
+++ global.d.ts
@@ -1,5 +1,5 @@
 import type { Schema } from "prosemirror-model";
-import type { ExtraPluginsSlots } from "@/keystatic/libs/plugins";
+import type { ExtraPluginsSlots } from "@/keystatic/plugins";
 
 declare global {
 	var __KEYSTATIC_EXTRA_PM_PLUGINS__: (schema: Schema) => ExtraPluginsSlots | ExtraPluginsSlots["beforeKeydown"];
