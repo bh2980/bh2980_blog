78c57cf37c9f632efc0c734bf3a795d52b98c649
bh2980 <bh2980@naver.com>
2026-01-28 22:15:07 +0900

    feat: MDX 후처리 훅 타입 정의 추가

--- global.d.ts
+++ global.d.ts
@@ -1,6 +1,16 @@
 import type { Schema } from "prosemirror-model";
 import type { ExtraPluginsSlots } from "@/keystatic/plugins";
 
+type HookCtx = { slug?: string };
+
 declare global {
 	var __KEYSTATIC_EXTRA_PM_PLUGINS__: (schema: Schema) => ExtraPluginsSlots | ExtraPluginsSlots["beforeKeydown"];
+	var __KEYSTATIC_MDX_HOOKS__:
+		| {
+				beforeParse?: (mdx: string, ctx: HookCtx) => string | undefined;
+				afterParse?: (doc: unknown, ctx: HookCtx) => unknown | undefined;
+				beforeSerialize?: (mdxAst: unknown, ctx: HookCtx) => unknown | undefined;
+				afterSerialize?: (mdx: string, ctx: HookCtx) => string | undefined;
+		  }
+		| undefined;
 }
