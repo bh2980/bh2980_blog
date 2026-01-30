6563e30d3d1eca5579881a4697106da4dd6d5bf3
bh2980 <bh2980@naver.com>
2026-01-30 11:36:51 +0900

    feat: annotation 관련 플러그인 추가 및 inline code 제거

--- src/components/mdx/mdx-content.tsx
+++ src/components/mdx/mdx-content.tsx
@@ -1,4 +1,16 @@
-import { type CodeHikeConfig, recmaCodeHike, remarkCodeHike } from "codehike/mdx";
+import bash from "@shikijs/langs/bash";
+import css from "@shikijs/langs/css";
+import javascript from "@shikijs/langs/javascript";
+import jsonc from "@shikijs/langs/jsonc";
+import python from "@shikijs/langs/python";
+import scss from "@shikijs/langs/scss";
+import solidity from "@shikijs/langs/solidity";
+import sql from "@shikijs/langs/sql";
+import tsTags from "@shikijs/langs/ts-tags";
+import tsx from "@shikijs/langs/tsx";
+import yaml from "@shikijs/langs/yaml";
+import oneDarkPro from "@shikijs/themes/one-dark-pro";
+
 import { compileMDX, MDXRemote, type MDXRemoteProps } from "next-mdx-remote/rsc";
 import rehypeAutolinkHeadings from "rehype-autolink-headings";
 import rehypeMermaid from "rehype-mermaid";
@@ -6,8 +18,13 @@ import rehypeSlug from "rehype-slug";
 import remarkBreaks from "remark-breaks";
 import remarkFlexibleToc, { type HeadingDepth, type TocItem } from "remark-flexible-toc";
 import remarkGfm from "remark-gfm";
+import { getSingletonHighlighterCore } from "shiki/core";
+import { createJavaScriptRegexEngine } from "shiki/engine/javascript";
+
+import { annotationConfig } from "@/keystatic/libs/serialize-annotations";
 import { rehypeMermaidDarkClass } from "@/libs/rehype/rehype-mermaid-dark-class";
-import { remarkCodeBlockAnnotation } from "@/libs/remark/remark-code-block-annotation";
+import { rehypeShikiDecorationRender } from "@/libs/rehype/rehype-shiki-decoration-render";
+import { remarkAnnotationToShikiDecoration } from "@/libs/remark/remark-annotation-to-decoration";
 import { remarkMermaidComponentToCode } from "@/libs/remark/remark-mermaid-component-to-code";
 import { a } from "./a";
 import { Callout } from "./callout";
@@ -17,6 +34,12 @@ import { Collapsible } from "./collapsible";
 import { Column, Columns } from "./columns";
 import { Tab, Tabs } from "./tabs";
 import { Tooltip } from "./tooltip";
+
+const highlighter = await getSingletonHighlighterCore({
+	themes: [oneDarkPro],
+	langs: [tsTags, javascript, tsx, css, scss, python, solidity, jsonc, yaml, sql, bash],
+	engine: createJavaScriptRegexEngine(),
+});
 
 interface MDXContentProps {
 	source: string;
@@ -33,9 +56,8 @@ export default function MDXContent({ source, options }: MDXContentProps) {
 			options={{
 				...options,
 				mdxOptions: {
-					remarkPlugins: [remarkBreaks, remarkGfm, [remarkCodeHike, chConfig]],
+					remarkPlugins: [remarkBreaks, remarkGfm],
 					rehypePlugins: [rehypeSlug, rehypeAutolinkHeadings],
-					recmaPlugins: [[recmaCodeHike, chConfig]],
 				},
 			}}
 			components={{
@@ -61,47 +83,37 @@ export const renderMDX = async (source: string) => {
 			mdxOptions: {
 				remarkPlugins: [
 					remarkMermaidComponentToCode,
-					remarkCodeBlockAnnotation,
+					[remarkAnnotationToShikiDecoration, annotationConfig],
 					remarkBreaks,
 					remarkGfm,
-					[remarkCodeHike, chConfig],
 					[remarkFlexibleToc, { tocRef, maxDepth: 3 }],
 				],
 				rehypePlugins: [
 					rehypeSlug,
 					rehypeAutolinkHeadings,
+					[rehypeShikiDecorationRender, { highlighter, theme: oneDarkPro }],
 					[rehypeMermaid, { strategy: "img-svg", dark: true }],
 					rehypeMermaidDarkClass,
 				],
-				recmaPlugins: [[recmaCodeHike, chConfig]],
 			},
 		},
 		components: {
+			a,
+			Callout,
+			Collapsible,
+			Columns,
+			Column,
+			CodeBlock,
 			Code,
 			CodeWithTooltips,
 			PureMdx: MDXContent,
-			Callout,
-			Collapsible,
-			code: InlineCode,
 			Tooltip,
-			a,
-			Columns,
-			Column,
 			Tabs,
 			Tab,
-			CodeBlock,
 		},
 	});
 
 	const toc = tocRef.map((item) => ({ ...item, depth: (item.depth - 2) as HeadingDepth }));
 
 	return { content, toc };
-};
-
-const chConfig: CodeHikeConfig = {
-	components: { code: "Code" },
-	ignoreCode: (codeblock) => codeblock.lang === "mermaid",
-	syntaxHighlighting: {
-		theme: "dark-plus",
-	},
 };
