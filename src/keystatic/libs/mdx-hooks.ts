b2796f6c35f94da0b9b1ce17eeb7cb5437b29ae7
bh2980 <bh2980@naver.com>
2026-01-30 13:52:57 +0900

    refactor: 불필요하게 선언된 ks 훅 제거

--- src/keystatic/libs/mdx-hooks.ts
+++ src/keystatic/libs/mdx-hooks.ts
@@ -2,21 +2,13 @@ import { walkOnlyInsideCodeFence } from "./parse-annotations";
 import { annotationConfig, walkOnlyInsideCodeblock } from "./serialize-annotations";
 
 globalThis.__KEYSTATIC_MDX_HOOKS__ = {
-	beforeParse(mdxAst) {
+	afterMarkdownParse(mdxAst) {
 		walkOnlyInsideCodeFence(mdxAst, annotationConfig);
 		return mdxAst;
-	},
-
-	afterParse(doc) {
-		return doc;
 	},
 
 	beforeSerialize(mdxAst) {
 		walkOnlyInsideCodeblock(mdxAst, annotationConfig);
 		return mdxAst;
-	},
-
-	afterSerialize(mdx) {
-		return mdx;
 	},
 };
