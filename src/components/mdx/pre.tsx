cd4286abbe6a96f1f94aa173a56cd92ee1fdad8c
bh2980 <bh2980@naver.com>
2026-02-02 00:28:02 +0900

    fix: light 테마에서 line number가 안나오는 문제 수정

--- src/components/mdx/pre.tsx
+++ src/components/mdx/pre.tsx
@@ -4,9 +4,9 @@ import { cn } from "@/utils/cn";
 import { CopyButton } from "./copy-button.client";
 
 type PreProps = PropsWithChildren<{
-	title?: string;
 	showLineNumbers?: boolean;
 	code: string;
+	title?: string;
 	className?: string;
 	style?: CSSProperties;
 }>;
@@ -17,28 +17,33 @@ export const Pre = async ({ children, title, showLineNumbers, code, className, s
 	const showTitlebar = filePath && filePath.length > 0;
 
 	return (
-		<div className="group relative flex flex-col">
-			{showTitlebar && (
-				<div className="flex items-center gap-1 rounded-t-xl bg-slate-600 px-3 py-1.5 text-slate-300 text-sm">
-					{filePath.map((folder, index, arr) => {
-						const isFile = arr.length - 1 === index;
+		<div className="group relative flex flex-col overflow-hidden rounded-xl border dark:border-none">
+			<div
+				className={cn(
+					"peer",
+					"hidden items-center gap-1 border-b bg-gray-100 px-3 py-1.5 text-sm dark:bg-gray-700 dark:text-slate-300",
+					showTitlebar && "flex",
+				)}
+				data-title={title}
+			>
+				{filePath?.map((folder, index, arr) => {
+					const isFile = arr.length - 1 === index;
 
-						return isFile ? (
-							<span key={folder} className="inline-flex items-center gap-1 font-semibold text-slate-50">
+					return isFile ? (
+						<span key={folder} className="inline-flex items-center gap-1 font-semibold dark:text-slate-50">
+							{folder}
+						</span>
+					) : (
+						<>
+							<span key={folder} className="inline-flex items-center gap-1">
+								<Folder size={16} className="stroke-2" />
 								{folder}
 							</span>
-						) : (
-							<>
-								<span key={folder} className="inline-flex items-center gap-1">
-									<Folder size={16} className="stroke-2" />
-									{folder}
-								</span>
-								<span>/</span>
-							</>
-						);
-					})}
-				</div>
-			)}
+							<span>/</span>
+						</>
+					);
+				})}
+			</div>
 			<pre
 				className={cn(
 					"relative overflow-x-auto whitespace-pre rounded-xl",
@@ -50,7 +55,7 @@ export const Pre = async ({ children, title, showLineNumbers, code, className, s
 			>
 				{children}
 			</pre>
-			<CopyButton text={code} className="top-2 lg:hidden lg:group-hover:block" />
+			<CopyButton text={code} className="peer-data-title:top-10! lg:hidden lg:group-hover:block" />
 		</div>
 	);
 };
