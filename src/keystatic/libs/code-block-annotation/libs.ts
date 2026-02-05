93817c5e3e9ba6581cbad3ad06559cdb869f367d
bh2980 <bh2980@naver.com>
2026-02-05 21:17:44 +0900

    feat: build~함수들을 create로 변경하고 crreate*Node 함수 추가

--- src/keystatic/libs/code-block-annotation/libs.ts
+++ src/keystatic/libs/code-block-annotation/libs.ts
@@ -1,11 +1,15 @@
-import type { Break, Node, Root, Text } from "mdast";
-import type { MdxJsxTextElement } from "mdast-util-mdx-jsx";
+import { parse } from "acorn";
+import type { Break, Node, PhrasingContent, Root, Text } from "mdast";
+import type { MdxJsxAttribute, MdxJsxAttributeValueExpression, MdxJsxTextElement } from "mdast-util-mdx-jsx";
 import { ANNOTATION_TYPE_DEFINITION } from "./constants";
-import type { AnnotationConfig, AnnotationRegistry, AnnotationType } from "./types";
-
-export const isText = (node: Node): node is Text => node.type === "text";
-export const isBreak = (node: Node): node is Break => node.type === "break";
-export const isMDXJSXTextElement = (node: Node): node is MdxJsxTextElement => node.type === "mdxJsxTextElement";
+import type {
+	Annotation,
+	AnnotationAttr,
+	AnnotationConfig,
+	AnnotationEvent,
+	AnnotationRegistry,
+	AnnotationType,
+} from "./types";
 
 export const hasChildren = (node: Node | Root): node is Node & { children: Node[] } => "children" in node;
 
@@ -13,7 +17,7 @@ export function getTypePair<T extends AnnotationType>(type: T) {
 	return { type, ...ANNOTATION_TYPE_DEFINITION[type] } as { type: T } & (typeof ANNOTATION_TYPE_DEFINITION)[T];
 }
 
-export const buildAnnotationRegistry = (annotationConfig?: AnnotationConfig) => {
+export const createAnnotationRegistry = (annotationConfig?: AnnotationConfig) => {
 	if (!annotationConfig) {
 		throw new Error("[buildAnnotationRegistry] ERROR : annotationConfig is required");
 	}
@@ -45,4 +49,111 @@ export const buildAnnotationRegistry = (annotationConfig?: AnnotationConfig) => 
 	}
 
 	return registry;
+};
+
+export const isText = (node: Node): node is Text => node.type === "text";
+export const isBreak = (node: Node): node is Break => node.type === "break";
+export const isMDXJSXTextElement = (node: Node): node is MdxJsxTextElement => node.type === "mdxJsxTextElement";
+
+export const createBreakNode = (): Break => ({
+	type: "break",
+});
+
+export const createTextNode = (value: string): Text => ({
+	type: "text",
+	value,
+});
+
+export const createMdastNode = (name: string, children: PhrasingContent[] = []) =>
+	({ type: name, children }) as PhrasingContent;
+
+const createMdxJsxAttribute = (name: string, value: any): MdxJsxAttribute => ({
+	type: "mdxJsxAttribute",
+	name,
+	value,
+});
+
+export const createMdxJsxTextElementNode = (
+	name: string,
+	attributes: AnnotationAttr[] = [],
+	children: PhrasingContent[] = [],
+): MdxJsxTextElement => {
+	return {
+		type: "mdxJsxTextElement",
+		name,
+		attributes: attributes.map((attr) => createMdxJsxAttribute(attr.name, attr.value)),
+		children,
+	};
+};
+
+export const createMdxJsxAttributeValueExpression = (value: unknown): MdxJsxAttributeValueExpression => {
+	const json = JSON.stringify(value);
+
+	const program = parse(`(${json})`, {
+		ecmaVersion: "latest",
+		sourceType: "module",
+	});
+
+	const firstStmt = program.body[0];
+	if (!firstStmt || firstStmt.type !== "ExpressionStatement") {
+		throw new Error("Failed to parse expression: not an ExpressionStatement");
+	}
+
+	const expression = firstStmt.expression;
+
+	return {
+		type: "mdxJsxAttributeValueExpression",
+		value: json,
+		data: {
+			estree: {
+				type: "Program",
+				sourceType: "module",
+				body: [
+					{
+						type: "ExpressionStatement",
+						expression,
+					},
+				] as any,
+			},
+		},
+	};
+};
+
+export const createEvents = (annotations: Annotation[]) => {
+	const event = annotations
+		.flatMap((anntation) => {
+			const startEvent: AnnotationEvent = { kind: "open", anno: anntation, pos: anntation.range.start };
+			const endEvent: AnnotationEvent = { kind: "close", anno: anntation, pos: anntation.range.end };
+
+			if (startEvent.pos === endEvent.pos) {
+				return [];
+			}
+
+			return [startEvent, endEvent];
+		})
+		.sort((a, b) => {
+			if (a.pos !== b.pos) {
+				return a.pos - b.pos;
+			}
+
+			if (a.kind !== b.kind) {
+				return a.kind.localeCompare(b.kind);
+			}
+
+			if (a.kind === "open" && a.anno.range.end !== b.anno.range.end) {
+				return b.anno.range.end - a.anno.range.end;
+			}
+
+			if (a.kind === "close" && a.anno.range.start !== b.anno.range.start) {
+				return b.anno.range.start - a.anno.range.start;
+			}
+
+			if (a.anno.typeId !== b.anno.typeId) {
+				return a.anno.typeId - b.anno.typeId;
+			}
+
+			return a.anno.order - b.anno.order;
+		});
+
+	return event;
 };
