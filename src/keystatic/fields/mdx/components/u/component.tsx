4a861ce0d350eae866d4ea45dd4d69c90fd16759
bh2980 <bh2980@naver.com>
2026-02-08 01:13:33 +0900

    feat: underline을 u 태그로 렌더링하고 className 추가

--- src/keystatic/fields/mdx/components/u/component.tsx
+++ src/keystatic/fields/mdx/components/u/component.tsx
@@ -1,11 +1,10 @@
-// editor-callout.tsx
 import { mark } from "@keystatic/core/content-components";
 import { Underline } from "lucide-react";
 
-// TODO : strong, em, del, sup, sub 추가
-// TODO : u 태그로 교체
-export const underline = mark({
+export const u = mark({
 	label: "밑줄",
 	icon: <Underline />,
 	schema: {},
+	tag: "u",
+	className: "underline-offset-4",
 });
