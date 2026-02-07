b9d2255c07e8a3d8d909b2297d327e428dbfd69f
bh2980 <bh2980@naver.com>
2026-02-08 02:10:21 +0900

    chore: TODO 제거

--- src/keystatic/fields/mdx/components/strong/component.tsx
+++ src/keystatic/fields/mdx/components/strong/component.tsx
@@ -1,7 +1,6 @@
 import { mark } from "@keystatic/core/content-components";
 import { Bold } from "lucide-react";
 
-// TODO : strong, em, del, sup, sub 추가
 export const strong = mark({
 	label: "굵게",
 	icon: <Bold />,
