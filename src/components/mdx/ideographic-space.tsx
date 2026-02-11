a08e6aa2f07777dbfa37c786b1c0a7676f7cba9f
Janghun Lee <74360958+bh2980@users.noreply.github.com>
2026-02-12 05:25:02 +0900

    에디터 및 mdx에 전각 공백 컴포넌트 추가 (#95)

---
+++ src/components/mdx/ideographic-space.tsx
@@ -0,0 +1,3 @@
+export const IdeographicSpace = () => {
+	return <span>ㅤ</span>;
+};
---
+++ src/keystatic/fields/mdx/components/ideographic-space/component.tsx
@@ -0,0 +1,12 @@
+
+import { inline } from "@keystatic/core/content-components";
+import { Subscript } from "lucide-react";
+
+export const IdeographicSpace = inline({
+	label: "전각 공백",
+	icon: <Subscript />,
+	schema: {},
+	NodeView() {
+		return <span className="inline-block h-5 w-3 rounded-xs border border-yellow-500" />;
+	},
+});
