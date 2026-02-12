2aec2f4e18f95f81ae28f46c9654cd425576c7b8
bh2980 <bh2980@naver.com>
2026-02-13 00:39:16 +0900

    fix: keystatic mdx 이미지 옵션 유지 및 게시글 에셋 경로 정리

--- src/keystatic/fields/mdx/mdx.ts
+++ src/keystatic/fields/mdx/mdx.ts
@@ -1,11 +1,14 @@
 import { fields } from "@keystatic/core";
 import { components } from "./components";
 
-export const mdx = (_args: Parameters<typeof fields.mdx>[0]) => {
+export const mdx = (args: Parameters<typeof fields.mdx>[0]) => {
+	const options = args.options ?? {};
+
 	return fields.mdx({
-		..._args,
+		...args,
 		components,
 		options: {
+			...options,
 			heading: [2, 3],
 			bold: false,
 			italic: false,
