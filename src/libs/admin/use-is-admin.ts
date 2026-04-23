04400db43efb6730dcce6873614e5cac799bee18
bh2980 <bh2980@naver.com>
2026-04-23 15:27:16 +0900

    fix: useIsAdmin 훅에서 isAdmin 기본 타입 지정

--- src/libs/admin/use-is-admin.ts
+++ src/libs/admin/use-is-admin.ts
@@ -1,7 +1,7 @@
 import { useCallback, useEffect, useState } from "react";
 
 export const useIsAdmin = () => {
-	const [isAdmin, setIsAdmin] = useState();
+	const [isAdmin, setIsAdmin] = useState(false);
 
 	const checkIsAdmin = useCallback(async () => {
 		const isAdmin = await fetch("/api/admin/access")
